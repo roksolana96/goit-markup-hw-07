e444b22c1b2169f38b915bc40fc46ee6edf9183e
Roksolana96 <roxolana.busko@gmail.com>
2022-12-09 23:28:45 +0100

    mob tablet comp

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,18 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
+    menuBtnRef.addEventListener("click", () => {
+      const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  
+      menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.setAttribute("aria-expanded", !expanded);
+  
+      mobileMenuRef.classList.toggle("is-open");
+    });
+  })();
